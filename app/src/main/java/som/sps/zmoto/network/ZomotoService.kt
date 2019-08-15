8a7368f82a0ec7f32495b45e0a5dbe018ae0c476
suraj <spsingh0002@gmail.com>
2019-08-15 14:29:46 +0530

    location api

--- app/src/main/java/som/sps/zmoto/network/ZomotoService.kt
+++ app/src/main/java/som/sps/zmoto/network/ZomotoService.kt
@@ -1,15 +1,25 @@
 package som.sps.zmoto.network
 
-import kotlinx.coroutines.Deferred
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Headers
+import retrofit2.http.Query
 import som.sps.zmoto.model.CategoriesResponse
+import som.sps.zmoto.model.LocationSuggestionResponse
 
 interface ZomotoService {
 
     @Headers("user-key:${Constants.key}")
     @GET("categories")
     fun getCategories(): Call<CategoriesResponse>
+
+    @Headers("user-key:${Constants.key}")
+    @GET("locations")
+    fun getLocationSuggestions(
+        @Query("query")  query:String,
+        @Query("lat") lat:Double?=Constants.lat,
+        @Query("lon") lon:Double?=Constants.longitute,
+        @Query("count") results:Int=10
+    ): Call<LocationSuggestionResponse>
+
 }
