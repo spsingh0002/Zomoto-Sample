3dc437b7004f86ba178df711f726be618587c5ec
suraj <spsingh0002@gmail.com>
2019-08-17 10:11:07 +0530

    add highlights

--- app/src/main/java/som/sps/zmoto/adapters/BindingAdapters.kt
+++ app/src/main/java/som/sps/zmoto/adapters/BindingAdapters.kt
@@ -1,6 +1,7 @@
 package som.sps.zmoto.adapters
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -8,11 +9,39 @@ import com.google.android.material.chip.Chip
 import som.sps.zmoto.R
 import som.sps.zmoto.listeners.SearchSelectionListener
 import som.sps.zmoto.model.LocationSuggestionResponse
+import som.sps.zmoto.model.Restaurant
 
 class BindingAdapters {
 
 
     companion object {
+
+        @JvmStatic
+        @BindingAdapter(value = arrayOf("app:averageCost"), requireAll = true)
+        fun bindAverageCost(textView: TextView, restaurant: Restaurant?) {
+            textView.text =
+                "Average ${restaurant?.averageCostForTwo}${restaurant?.currency} for ${restaurant?.priceRange}"
+
+        }
+
+        @JvmStatic
+        @BindingAdapter("app:address")
+        fun bindAddress(textView: TextView, restaurant: Restaurant?) {
+            textView.text =
+                "${restaurant?.location?.address}\n${restaurant?.location?.locality}\n${restaurant?.location?.city}"
+
+        }
+
+        @JvmStatic
+        @BindingAdapter(" app:highlight")
+        fun bindHighlightsText(
+            textView: TextView,
+            highlights: List<String>?
+        ) {
+            textView.text ="\nHighLights: \n  ${ highlights?.joinToString(",")}"
+
+        }
+
 
         @JvmStatic
         @BindingAdapter(" app:highlight")
